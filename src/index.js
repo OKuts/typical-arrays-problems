70ca3923e934722c54324813f0acdce8db7ba1da
OKuts <kuts.ol.vas@gmail.com>
2020-10-01 18:20:49 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-
-exports.min = function min (array) {
-  return 0;
+exports.min = function min(array) {
+    return Array.isArray(array) && array.length > 0 ? array.sort((a, b) => a - b)[0] : 0;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max(array) {
+    return Array.isArray(array) && array.length > 0 ? array.sort((a, b) => b - a)[0] : 0;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg(array) {
+    return Array.isArray(array) && array.length > 0 ? array.reduce((sum, el) => sum + el) / array.length : 0;
 }
